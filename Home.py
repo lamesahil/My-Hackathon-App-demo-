0fe8e7fb383439261d3758e9456a6f7673789240
Sahil Tiwari <iamsahilt20@gmail.com>
2026-02-01 12:07:01 +0530

    Enhance Home.py with footer and styling
    Added footer with team information and copyright notice.

--- Home.py
+++ Home.py
@@ -6,3 +6,14 @@ Ye humara **All-in-One Solution**.
 👈 In the sidebar menu, you can navigate to different pages.
 """)
 st.info("We are ready for Hackathon 🦁")
+
+st.markdown("---")
+st.markdown(
+    """
+    <div style="text-align: center; font-size: 14px; color: #888;">
+        <p>Made with ❤️ by <b>Team QuadCore</b> for SPIT Hackathon 2026</p>
+        <p>© 2026 All Rights Reserved | 📍 Mumbai, India</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
