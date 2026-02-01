f62cd4e60581d6d55eb61118388df8c412dbc2e1
Sahil Tiwari <iamsahilt20@gmail.com>
2026-02-01 11:34:28 +0530

    Fix formatting in Home.py for Streamlit app

--- Home.py
+++ Home.py
@@ -2,7 +2,8 @@ import streamlit as st
 st.title("🏠 Welcome to Team QuadCore")
 st.image("https://media.giphy.com/media/l41YmQjOz9qg2EJfG/giphy.gif", width=300) # Fun GIF
 st.write("""
-Ye humara **All-in-One Solution**.
+**All-in-One Solution**.
 👈 In the sidebar menu, you can navigate to different pages.
 """)
 st.info("We are ready for Hackathon 🦁")
+
